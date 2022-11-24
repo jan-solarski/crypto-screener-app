2f1c07507290e8a30f99d3c2c7793998b7fbe535
Jan Solarski <jan.solarski@gmail.com>
2022-11-24 12:56:57 +0100

    add saved page

--- src/components /Logo.jsx
+++ src/components /Logo.jsx
@@ -5,7 +5,7 @@ export const Logo = () => {
   return (
     <Link
       to="/"
-      className="absolute top-[1.5rem] left-[1.5rem] [text-decoration:none] text-lg text-cyan flex items-center"
+      className="absolute sm:top-[1.5rem] top-1[rem] sm:left-[1.5rem] left-[1rem] [text-decoration:none] sm:text-lg text-md text-cyan flex items-center cursor-pointer"
     >
       <img src={logoSvg} alt="Logo" />
       <span>CryptoBucks</span>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,20 @@ import { Logo } from "../components /Logo.jsx";
 import { Navigation } from "../components /Navigation.jsx";
 import { CryptoProvider } from "../context/CryptoContext.jsx";
 import { TrendingProvider } from "../context/TrendingContext.jsx";
+import { StorageProvider } from "../context/StorageContext.jsx";
 
 export const Home = () => {
   return (
     <CryptoProvider>
       <TrendingProvider>
-        <main className="w-full h-full flex flex-col first-letter:content-center items-center relative text-white">
-          <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
-          <Logo />
-          <Navigation />
-          <Outlet />
-        </main>
+        <StorageProvider>
+          <main className="w-full h-full flex flex-col first-letter:content-center items-center relative text-white">
+            <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
+            <Logo />
+            <Navigation />
+            <Outlet />
+          </main>
+        </StorageProvider>
       </TrendingProvider>
     </CryptoProvider>
   );
